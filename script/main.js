a0158f30ec722dd2cadad4d82de1bed143a82930
Nathan Soussana <nathan.soussana@gmail.com>
2023-10-06 13:35:21 +0200

    Removed sticky navbar due to bugs

--- script/main.js
+++ script/main.js
@@ -1,18 +1,3 @@
-// Sticky navbar
-
-const navbar = document.querySelector(".navbar");
-const navbarOffsetTop = navbar.offsetTop;
-
-function toggleStickyNavbar() {
-  if (window.pageYOffset >= navbarOffsetTop) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-window.addEventListener("scroll", toggleStickyNavbar);
-
 const hamburgerIcon = document.getElementById("navbar_hamburger")
 
 hamburgerIcon.addEventListener("click", function() {
